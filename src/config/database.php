5c456a1f35ab85de62e6ada1507f13f71549079f
JuanNeciosup <juanneciosup455@gmail.com>
2025-03-27 07:38:37 -0500

    database

--- src/config/database.php
+++ src/config/database.php
@@ -2,14 +2,13 @@
 // Configuración de la base de datos
 $host = "localhost";
 $dbname = "gestion_piscicola";
-$user = "root";  // Cambia esto si tienes otro usuario
-$password = "";  // Si tienes una contraseña, agrégala aquí
+$user = "root";  
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    die("❌ Error de conexión: " . $e->getMessage());
+    die("Error de conexión: " . $e->getMessage());
 }
 ?>
